74b8ee8da913df9d679200fd804e5df84d0a4278
TERESIA012 <kingoriteresia@gmail.com>
2021-10-29 16:09:14 +0300

    add function register

--- myhood/views.py
+++ myhood/views.py
@@ -1,6 +1,6 @@
-from hood.models import Business, Post, Profile, Neighbourhood
+from myhood.models import Business, Post, Profile, Neighbourhood
 from django.contrib.auth.models import User
-from hood.forms import NewBusinessForm, NewHoodForm, PostForm, SignUpForm, UpdateProfileForm, UpdateUserForm
+from myhood.forms import NewBusinessForm, NewHoodForm, PostForm, SignUpForm, UpdateProfileForm, UpdateUserForm
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render, redirect
@@ -8,3 +8,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
