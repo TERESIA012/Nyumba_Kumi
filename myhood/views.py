3f70a348bba0b5b046c5385c2fac3498ffc58b3d
TERESIA012 <kingoriteresia@gmail.com>
2021-10-29 15:21:30 +0300

    add views import

--- myhood/views.py
+++ myhood/views.py
@@ -1,3 +1,10 @@
+from hood.models import Business, Post, Profile, Neighbourhood
+from django.contrib.auth.models import User
+from hood.forms import NewBusinessForm, NewHoodForm, PostForm, SignUpForm, UpdateProfileForm, UpdateUserForm
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
